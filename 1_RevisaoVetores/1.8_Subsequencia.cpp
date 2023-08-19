1ab797d63ac712a1abd5844830d17823e0b395a0
danilojmarins <danilojmarins@hotmail.com>
2023-08-19 19:05:29 -0300

    Subsequencia.

--- 1_RevisaoVetores/1.8_Subsequencia.cpp
+++ 1_RevisaoVetores/1.8_Subsequencia.cpp
@@ -4,5 +4,31 @@ int main()
 {
     int vetor[5] = {3,10,2,1,20};
 
+    int counter = 0;
+
+    int agreagado = 0;
+
+    printf("Maior subsequência crescente: \n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (vetor[j] >= vetor[i] && j >= i)
+            {
+                printf("%d ", vetor[j]);
+            }
+        }
+        
+        if (counter > agreagado)
+        {
+            agreagado = counter;
+        }
+
+        counter = 0;
+    }
+
+    printf("%d \n", agreagado);
+
     return 0;
 }
