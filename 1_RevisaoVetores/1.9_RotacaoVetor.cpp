981a2e0fdc2222e67d41c89dfeae2cef5b290eae
danilojmarins <danilojmarins@hotmail.com>
2023-08-20 19:49:31 -0300

    Rotação Vetor.

--- 1_RevisaoVetores/1.9_RotacaoVetor.cpp
+++ 1_RevisaoVetores/1.9_RotacaoVetor.cpp
@@ -2,20 +2,33 @@
 
 int main()
 {
-    int vetor[5] = {1,2,3,4,5};
-    int rotacao = 3;
+    int tamanho = 5;
+    int vetor[tamanho] = {1,2,3,4,5};
+    int rotacao = 2;
+    int vetorRotacionado[tamanho];
 
-    for (int i = 0; i < rotacao; i++)
+    int index;
+
+    for (int i = 0; i < tamanho; i++)
     {
-        int temp = vetor[i];
-        vetor[i] = vetor[5 - rotacao + i];
-        vetor[5 - i -1] = temp;
+        int index = i + rotacao;
+
+        if (index > tamanho - 1)
+        {
+            index = index - tamanho;
+        }
+
+        vetorRotacionado[index] = vetor[i];
     }
 
-    for (int j = 0; j < 5; j++)
+    printf("Vetor Rotacionado: ");
+
+    for (int j = 0; j < tamanho; j++)
     {
-        printf("%d ", vetor[j]);
+        printf("%d ", vetorRotacionado[j]);
     }
+
+    printf("\n");
 
     return 0;
 }
